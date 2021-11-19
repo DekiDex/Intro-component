9d6e7147523db34500a4b17fde14ff831c2b2641
DekiDex <89651615+DekiDex@users.noreply.github.com>
2021-11-19 19:42:03 +0100

    intro

---
+++ javascript/script.js
@@ -0,0 +1,59 @@
+// inputs
+let firstName = document.getElementById('first-name')
+let lastName = document.getElementById('last-name')
+let email = document.getElementById('email')
+let password = document.getElementById('pass')
+
+// button
+let btn = document.getElementById('btn')
+
+// error messages and containers
+let errorMessageList = document.querySelectorAll('.error')
+let errorIcons = document.querySelectorAll('.err-icon')
+let errorContainers = document.querySelectorAll('.input-container')
+
+btn.addEventListener('click', () => {
+
+    const regex_email = /\S+@\S+\.\S+/;
+
+    if ( firstName.value.length == 0 ){
+        errorMessageList[0].style.display = 'block'
+        errorIcons[0].style.display = 'block'
+        errorContainers[0].style.border = '2px solid red'
+    } else{
+        errorMessageList[0].style.display = 'none'
+        errorIcons[0].style.display = 'none'
+        errorContainers[0].style.border = '2px solid lightgrey'
+    }
+
+    if ( lastName.value.length == 0 ){
+        errorMessageList[1].style.display = 'block'
+        errorIcons[1].style.display = 'block'
+        errorContainers[1].style.border = '2px solid red'
+    } else{
+        errorMessageList[1].style.display = 'none'
+        errorIcons[1].style.display = 'none'
+        errorContainers[1].style.border = '2px solid lightgrey'
+    }
+
+    if ( email.value.length == 0 || regex_email.test(email.value) == false){
+        errorMessageList[2].style.display = 'block'
+        errorIcons[2].style.display = 'block'
+        errorContainers[2].style.border = '2px solid red'
+    } else{
+        errorMessageList[2].style.display = 'none'
+        errorIcons[2].style.display = 'none'
+        errorContainers[2].style.border = '2px solid lightgrey'
+    }
+
+    if ( password.value.length == 0 ){
+        errorMessageList[3].style.display = 'block'
+        errorIcons[3].style.display = 'block'
+        errorContainers[3].style.border = '2px solid red'
+    } else{
+        errorMessageList[3].style.display = 'none'
+        errorIcons[3].style.display = 'none'
+        errorContainers[3].style.border = '2px solid lightgrey'
+    }
+
+})
